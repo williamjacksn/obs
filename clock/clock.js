b2c8fbd14ce29adba664cce0ff1889d8f9aa5d0b
William Jackson <william@subtlecoolness.com>
2021-06-10 14:06:06 -0500

    Add opacity parameter

--- clock/clock.js
+++ clock/clock.js
@@ -1,22 +1,29 @@
-const url_params = new URLSearchParams(window.location.search);
+const urlSearchParams = new URLSearchParams(window.location.search);
 
-let tz = 'America/Los_Angeles';
-if (url_params.has('tz')) {
-    tz = url_params.get('tz');
+let opacity = 0;
+if (urlSearchParams.has('opacity')) {
+    opacity = urlSearchParams.get('opacity');
 }
+
+document.body.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`
 
 let loc = 'Los Angeles, California';
-if (url_params.has('loc')) {
-    loc = url_params.get('loc');
+if (urlSearchParams.has('loc')) {
+    loc = urlSearchParams.get('loc');
 }
 
-const locEl = document.getElementById('location');
-locEl.textContent = loc;
+document.getElementById('location').textContent = loc;
+
+let tz = 'America/Los_Angeles';
+if (urlSearchParams.has('tz')) {
+    tz = urlSearchParams.get('tz');
+}
+
+const clockEl = document.getElementById('clock');
 
 function updateClock() {
     const now = new Date();
-    const div = document.getElementById('clock');
-    div.textContent = now.toLocaleTimeString([], {
+    clockEl.textContent = now.toLocaleTimeString([], {
         hour: 'numeric',
         minute: '2-digit',
         timeZone: tz
