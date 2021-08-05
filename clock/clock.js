3ed7e74741768258df95853b8cc9eab0c6f2b49c
William Jackson <william@subtlecoolness.com>
2021-08-05 12:18:20 -0500

    Clean up clock

--- clock/clock.js
+++ clock/clock.js
@@ -5,7 +5,8 @@ if (urlSearchParams.has('opacity')) {
     opacity = urlSearchParams.get('opacity');
 }
 
-document.body.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`
+const containerEl = document.getElementById('container');
+containerEl.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
 
 let loc = 'Los Angeles, California';
 if (urlSearchParams.has('loc')) {
